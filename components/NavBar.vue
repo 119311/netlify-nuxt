6851af3fbd9ef1aaee440dffdbccd00f667c1ad9
119311 <r119311@g.kobe-kosen.ac.jp>
2022-05-05 17:47:28 +0900

    test: チュートリアル終了

---
+++ components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nav>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/about">About</nuxt-link>
+    <nuxt-link to="/inspire">Inspire</nuxt-link>
+    </nav>
+  </div>
+</template>
